a3f29eaa6b0240509249b57f0194f69a81a0483a
Sandhu, Tanishq Singh <tanishq@gatech.edu>
2021-04-27 01:13:31 -0400

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,51 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+
+contract VaccinationRecord {
+
+    struct Patient {
+        string vaccineBrand;
+        uint firstDoseLotNum; 
+        bool firstDose;  // if true, that person received first vaccine
+        uint256 firstDoseDate; 
+        string firstDoseSite;
+        string firstDoseAddress;
+        uint secondDoseLotNum; 
+        bool secondDose;  // if true, that person received second vaccine
+        uint256 secondDoseDate; 
+        string secondDoseSite;
+        string secondDoseAddress;
+    }
+    
+    function vaccinationStatus() public view
+            returns (string status_)
+    {
+        if (firstDose && secondDose) {
+		status = "Fully Vaccinated";
+	} else if (firstDose) {
+         	status = "First Dose";
+	} else {
+		status = "Vaccination Not Started";
+	}
+
+	return status;
+    }
+
+    function firstVaccine(string fds, string fda, uint256 fdd, uint fdln) {
+	firstDose = true;
+	firstDoseLotNum = fdln; 
+	firstDoseDate = fdd; 
+ 	firstDoseSite = fds;
+	firstDoseAddress = fad;
+
+    }
+
+    function secondVaccine(string sds, string sda, uint256 sdd, uint sdln) {
+	secondDose = true;
+	secondDoseLotNum = sdln; 
+	secondDoseDate = sdd; 
+ 	secondDoseSite = sds;
+	secondDoseAddress = sad;
+
+    }
+}
